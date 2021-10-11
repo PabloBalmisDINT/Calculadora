d301fc3dbae559c917bfee5d23720d96ef8e559b
Pablo Martinez Aznar <pablo.martinezaznar@iesdoctorbalmis.com>
2021-10-11 19:19:35 +0200

    Termino el ejercicio

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button boton = (Button)sender;
+            pantalla.Text += boton.Tag.ToString();
+        }
     }
 }
